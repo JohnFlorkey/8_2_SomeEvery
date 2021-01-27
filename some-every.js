bccd4dc0533d30a2a3ef6b49da29267f5bc1b16b
John Florkey <john.florkey@gmail.com>
2021-01-26 20:21:44 -0500

    hasCertainValue working

--- some-every.js
+++ some-every.js
@@ -105,4 +105,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(value){
+        return value[key] === searchValue;
+    })
+}
